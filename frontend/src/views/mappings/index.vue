91f2f880cdbe7124d3acbaa70fae14999558076d
louis <191180776@qq.com>
2025-11-17 18:25:34 +0800

    调整映射管理页面表格列宽，优化显示效果
    - 调整客户名称列宽为250px，银行名称列宽为100px
    - 调整对方账户名称列宽为280px，银行账号列宽为220px
    - 优化表格布局，提升用户体验

--- frontend/src/views/mappings/index.vue
+++ frontend/src/views/mappings/index.vue
@@ -52,8 +52,8 @@
             style="width: 100%"
           >
             <el-table-column type="index" label="序号" width="60" />
-            <el-table-column prop="customer_name" label="客户名称" width="120" />
-            <el-table-column prop="bank_name" label="银行名称" width="120" />
+            <el-table-column prop="customer_name" label="客户名称" width="250" />
+            <el-table-column prop="bank_name" label="银行名称" width="100" />
             <el-table-column prop="date" label="日期列名" width="100" />
             <el-table-column prop="counterparty" label="对方户名" width="100" />
             <el-table-column prop="summary" label="摘要列名" width="100" />
@@ -142,11 +142,11 @@
             style="width: 100%"
           >
             <el-table-column type="index" label="序号" width="60" />
-            <el-table-column prop="customer_name" label="客户名称" width="150" />
+            <el-table-column prop="customer_name" label="客户名称" width="100" />
             <el-table-column prop="match_type" label="匹配方式" width="150" />
-            <el-table-column prop="counterparty_name" label="对方账户名称" width="200" />
-            <el-table-column prop="keywords" label="摘要关键字" width="200" />
-            <el-table-column prop="bank_account" label="银行账号" width="200" />
+            <el-table-column prop="counterparty_name" label="对方账户名称" width="280" />
+            <el-table-column prop="keywords" label="摘要关键字" width="120" />
+            <el-table-column prop="bank_account" label="银行账号" width="220" />
             <el-table-column prop="subject_code" label="会计科目编码" width="150" />
             <el-table-column label="操作" width="180" fixed="right">
               <template #default="{ row }">
